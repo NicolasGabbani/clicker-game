9529c021db9fe267253c29ed3812bfc3944d68fc
Nicolas Gabbani <nicolasgabbani@gmail.com>
2021-03-25 17:21:19 +0100

    add more success

--- src/data/success.js
+++ src/data/success.js
@@ -20,5 +20,23 @@ export default [
     buildName: 'mine',
     name: 'Avoir une mine',
     done: false
+  },
+  {
+    id: 'haveOne',
+    buildName: 'robot',
+    name: 'Avoir un robot',
+    done: false
+  },
+  {
+    id: 'haveOne',
+    buildName: 'usine',
+    name: 'Avoir une usine',
+    done: false
+  },
+  {
+    id: 'haveOne',
+    buildName: 'vaisseau',
+    name: 'Avoir un vaisseau',
+    done: false
   }
 ]
