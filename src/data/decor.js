789551154055558885d6beca8515828b2b1a775f
Nicolas Gabbani <nicolasgabbani@gmail.com>
2021-04-09 15:15:08 +0200

    decor number

--- src/data/decor.js
+++ src/data/decor.js
@@ -7,89 +7,89 @@ export default [
   },
   {
     cls: 'l3tsg01',
-    score: 15,
+    score: 2000,
     get: false,
     licorne: false
   },
   {
     cls: 'cb0nc4',
-    score: 30,
+    score: 50000,
     get: false,
     licorne: false
   },
   {
     cls: 'jml3sp0p',
-    score: 60,
+    score: 200000,
     get: false,
     licorne: false
   },
   {
     cls: 'j4im3l3sch4ts',
-    score: 80,
+    score: 1000000,
     get: false,
     licorne: false
   },
   {
     cls: 'cm4rr4nt',
-    score: 100,
+    score: 10000000,
     get: false,
     licorne: false
   },
   {
     cls: 'jss1l1c0rn3',
-    score: 115,
+    score: 25000000,
     get: false,
     licorne: false
   },
   {
     cls: 'g4m4d0n3gg',
-    score: 130,
+    score: 65000000,
     get: false,
     licorne: false
   },
   {
     cls: 'g0g4l4x1e',
-    score: 160,
+    score: 100000000,
     get: false
   },
   {
     cls: 'n3xtl3v3l',
-    score: 180,
+    score: 300000000,
     get: false
   },
   {
     cls: 'un1v3rs20uf',
-    score: 200,
+    score: 800000000,
     get: false
   },
   {
     cls: 'jss0ul4l0l',
-    score: 215,
+    score: 1250000000,
     get: false
   },
   {
     cls: 'j3v0l',
-    score: 230,
+    score: 2000000000,
     get: false
   },
   {
     cls: 'y0y0l30uf',
-    score: 280,
+    score: 2500000000,
     get: false
   },
   {
     cls: 'sky1sth3l1m1t',
-    score: 310,
+    score: 3125000000,
     get: false
   },
   {
     cls: 'n0m0rel1m1it',
-    score: 340,
+    score: 4000000000,
     get: false
   },
   {
     cls: 'sup3rbbl',
-    score: 370,
+    score: 1000000000000,
     get: false
   }
 ]
