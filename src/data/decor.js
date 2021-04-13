cf3695210554a15ef1d8324df4b66ef0d10e8be5
Nicolas Gabbani <nicolasgabbani@gmail.com>
2021-04-13 16:47:22 +0200

    new decor score

--- src/data/decor.js
+++ src/data/decor.js
@@ -7,89 +7,89 @@ export default [
   },
   {
     cls: 'l3tsg01',
-    score: 2000,
+    score: 20000,
     get: false,
     licorne: false
   },
   {
     cls: 'cb0nc4',
-    score: 50000,
+    score: 5000000,
     get: false,
     licorne: false
   },
   {
     cls: 'jml3sp0p',
-    score: 200000,
+    score: 100000000,
     get: false,
     licorne: false
   },
   {
     cls: 'j4im3l3sch4ts',
-    score: 1000000,
+    score: 100000000000,
     get: false,
     licorne: false
   },
   {
     cls: 'cm4rr4nt',
-    score: 10000000,
+    score: 100000000000000,
     get: false,
     licorne: false
   },
   {
     cls: 'jss1l1c0rn3',
-    score: 25000000,
+    score: 100000000000000000,
     get: false,
     licorne: false
   },
   {
     cls: 'g4m4d0n3gg',
-    score: 65000000,
+    score: 100000000000000000000,
     get: false,
     licorne: false
   },
   {
     cls: 'g0g4l4x1e',
-    score: 100000000,
+    score: 100000000000000000000000,
     get: false
   },
   {
     cls: 'n3xtl3v3l',
-    score: 300000000,
+    score: 100000000000000000000000000,
     get: false
   },
   {
     cls: 'un1v3rs20uf',
-    score: 800000000,
+    score: 100000000000000000000000000000,
     get: false
   },
   {
     cls: 'jss0ul4l0l',
-    score: 1250000000,
+    score: 100000000000000000000000000000000,
     get: false
   },
   {
     cls: 'j3v0l',
-    score: 2000000000,
+    score: 100000000000000000000000000000000000,
     get: false
   },
   {
     cls: 'y0y0l30uf',
-    score: 2500000000,
+    score: 100000000000000000000000000000000000000,
     get: false
   },
   {
     cls: 'sky1sth3l1m1t',
-    score: 3125000000,
+    score: 100000000000000000000000000000000000000000,
     get: false
   },
   {
     cls: 'n0m0rel1m1it',
-    score: 4000000000,
+    score: 100000000000000000000000000000000000000000000,
     get: false
   },
   {
     cls: 'sup3rbbl',
-    score: 1000000000000,
+    score: 100000000000000000000000000000000000000000000000,
     get: false
   }
 ]
