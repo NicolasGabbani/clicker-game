ef11a32d7259384282f5b9ead676cffa6b39f364
Nicolas Gabbani <nicolasgabbani@gmail.com>
2021-04-28 18:17:35 +0200

    fix typo

--- src/components/Builds.vue
+++ src/components/Builds.vue
@@ -1,36 +1,107 @@
 <template>
   <div class="nes-container is-rounded is-semi-white container-content">
     <p class="title">Fabriques à miam-miam</p>
-    <div class="container-scroll" data-simplebar data-simplebar-auto-hide="false">
-      <div v-for="(build, index) in builds" :key="build.name" class="build with-hover nes-container is-rounded" :class="{ disable: !build.buyable }" v-show="total >= build.visible" :ref='`build${index}`' @mouseenter="toggleInfo = index" @mouseleave="toggleInfo = null">
+    <div
+      class="container-scroll"
+      data-simplebar
+      data-simplebar-auto-hide="false"
+    >
+      <div
+        v-for="(build, index) in builds"
+        :key="build.name"
+        class="build with-hover nes-container is-rounded"
+        :class="{ disable: !build.buyable }"
+        v-show="total >= build.visible"
+        :ref="`build${index}`"
+        @mouseenter="toggleInfo = index"
+        @mouseleave="toggleInfo = null"
+      >
         <div class="build__stars">
-          <div class="build__stars-star" v-for="star in build.stars">
-            <i class="nes-icon star animate__animated animate__fadeIn animate__faster"></i>
+          <div
+            class="build__stars-star"
+            v-for="star in build.stars"
+            :key="star"
+          >
+            <i
+              class="nes-icon star animate__animated animate__fadeIn animate__faster"
+            ></i>
           </div>
         </div>
         <div class="build__avatar">
-          <div class="build__avatar-img nes-container is-rounded" :style="{backgroundImage: `url(${require(`@/assets/images/builds/unites-production-${index+1}.png`)})` }"></div>
+          <div
+            class="build__avatar-img nes-container is-rounded"
+            :style="{
+              backgroundImage: `url(${require(`@/assets/images/builds/unites-production-${
+                index + 1
+              }.png`)})`,
+            }"
+          ></div>
         </div>
         <div class="build-content">
           <p class="build__title">{{ build.name }}</p>
-          <p class="build__number animate__animated" :class="{animate__tada: index == addBuild}">{{ build.number }}</p>
-          <p style="display: none" :class="{buyable: build.buyable}"></p>
+          <p
+            class="build__number animate__animated"
+            :class="{ animate__tada: index == addBuild }"
+          >
+            {{ build.number }}
+          </p>
+          <p style="display: none" :class="{ buyable: build.buyable }"></p>
 
           <div class="build-btn-content container-grid-space">
             <div>
-              <button v-show="index == toggleInfo" class="nes-btn build-btn-info" @click.prevent.stop="openInfo(index)">voir info</button>
-              <p v-show="toggleInfo == null || index != toggleInfo" class="build__ps">+{{ this.renderNumeral(build.inc) }}<span class="price fraise"></span>/seconde</p>
+              <button
+                v-show="index == toggleInfo"
+                class="nes-btn build-btn-info"
+                @click.prevent.stop="openInfo(index)"
+              >
+                voir info
+              </button>
+              <p
+                v-show="toggleInfo == null || index != toggleInfo"
+                class="build__ps"
+              >
+                +{{ this.renderNumeral(build.inc)
+                }}<span class="price fraise"></span>/seconde
+              </p>
             </div>
-            <button class="nes-btn build-btn-buy" :class="{'is-success': build.buyable, 'is-error': !build.buyable, 'btn-disable': !build.buyable}" @click.prevent.stop="$emit('buyBuild', build); buildAdded(index);">{{ this.renderNumeral(build.price) }}<span class="price fraise very-small-fraise fraise-bonus"></span></button>
+            <button
+              class="nes-btn build-btn-buy"
+              :class="{
+                'is-success': build.buyable,
+                'is-error': !build.buyable,
+                'btn-disable': !build.buyable,
+              }"
+              @click.prevent.stop="$emit('buyBuild', build), buildAdded(index)"
+            >
+              {{ this.renderNumeral(build.price)
+              }}<span
+                class="price fraise very-small-fraise fraise-bonus"
+              ></span>
+            </button>
           </div>
-          
-          <div class="build-info nes-container is-white is-rounded" v-show="index == infoClose">
+
+          <div
+            class="build-info nes-container is-white is-rounded"
+            v-show="index == infoClose"
+          >
             <p class="title">Info</p>
-            <button class="nes-btn build-info-close" @click.prevent.stop="closeInfo"><i class="nes-icon close is-small"></i></button>
+            <button
+              class="nes-btn build-info-close"
+              @click.prevent.stop="closeInfo"
+            >
+              <i class="nes-icon close is-small"></i>
+            </button>
             <div class="lists">
               <ul class="nes-list is-disc">
-                <li>{{ Math.floor((build.inc*build.number * 100) / cps) || 0 }}% du total par seconde</li>
-                <li>{{ renderNumeral(build.total) }}<span class="fraise small-fraise fraise-bonus"></span>total depuis le début</li>
+                <li>
+                  {{ Math.floor((build.inc * build.number * 100) / cps) || 0 }}%
+                  du total par seconde
+                </li>
+                <li>
+                  {{ renderNumeral(build.total)
+                  }}<span class="fraise small-fraise fraise-bonus"></span>total
+                  depuis le début
+                </li>
               </ul>
             </div>
           </div>
@@ -40,125 +111,125 @@
   </div>
 </template>
 <script>
-import 'simplebar'
-import 'simplebar/dist/simplebar.css'
+import "simplebar"
+import "simplebar/dist/simplebar.css"
 export default {
-  name: 'Builds',
+  name: "Builds",
   props: {
     builds: Array,
     total: Number,
-    cps: Number
+    cps: Number,
   },
-  data(){
-    return{
+  data() {
+    return {
       addBuild: null,
       modulo: 10,
       infoClose: null,
-      toggleInfo: null
+      toggleInfo: null,
     }
   },
   methods: {
-    buildAdded(index){
+    buildAdded(index) {
       this.addBuild = index
     },
-    openInfo(index){
+    openInfo(index) {
       this.infoClose = index
     },
-    closeInfo(){
+    closeInfo() {
       this.infoClose = null
     },
-    renderNumeral(val){
-      if(val >= 1000000) return numeral(val).format('0.000a')
-      if(val % 1 !== 0) return numeral(val).format('0,0.0')
-      return numeral(val).format('0,0')
-    }
+    renderNumeral(val) {
+      if (val >= 1000000) return numeral(val).format("0.000a")
+      if (val % 1 !== 0) return numeral(val).format("0,0.0")
+      return numeral(val).format("0,0")
+    },
   },
   watch: {
-    addBuild: function(val){
-      if(this.$timer){
+    addBuild: function (val) {
+      if (this.$timer) {
         clearTimeout(this.$timer)
       }
       this.$timer = setTimeout(() => {
         this.addBuild = null
-      }, 500);
-    }
-  }
+      }, 500)
+    },
+  },
 }
 </script>
 
 <style lang="sass">
-  .build
-    position: relative !important
-    margin-bottom: 20px !important
-    padding: .5rem 1.5rem !important
-    font-size: .7rem
-    min-height: 105px
-    background: rgba(255,255,255,.1)
-    backdrop-filter: blur(10px)
-    transition: all var(--tr-du)
-    color: var(--clr-black)
-    &__stars
-      position: absolute
-      top: 70px
-      left: 0
-      z-index: 2
-      &-star
-        display: inline-block
-        i
-          margin: 0 !important
-    &__avatar
-      text-align: center
-      position: absolute
-      top: -7px
-      left: -8px
-      &-img
-        width: 90px
-        height: 90px
-        background-repeat: no-repeat
-        background-size: cover
-        background-position: center center
-    &-content
-      margin-left: 90px
-      &.build-placeholder
-        margin-left: 0
-        position: absolute
-        top: 0
-        left: 0
-        right: 0
-        bottom: 0
-    &__title
-      font-size: 1rem
-      width: 90%
-      font-weight: bold
-    &__ps
-      margin-bottom: 0
-    &__number
-      margin-bottom: 0
+.build
+  position: relative !important
+  margin-bottom: 20px !important
+  padding: .5rem 1.5rem !important
+  font-size: .7rem
+  min-height: 105px
+  background: rgba(255,255,255,.1)
+  backdrop-filter: blur(10px)
+  transition: all var(--tr-du)
+  color: var(--clr-black)
+  &__stars
+    position: absolute
+    top: 70px
+    left: 0
+    z-index: 2
+    &-star
+      display: inline-block
+      i
+        margin: 0 !important
+  &__avatar
+    text-align: center
+    position: absolute
+    top: -7px
+    left: -8px
+    &-img
+      width: 90px
+      height: 90px
+      background-repeat: no-repeat
+      background-size: cover
+      background-position: center center
+  &-content
+    margin-left: 90px
+    &.build-placeholder
+      margin-left: 0
       position: absolute
       top: 0
-      right: 20px
-      font-size: 2rem
-      color: var(--clr-grey)
-    &-btn-content
-      width: 100%
-    &-info
+      left: 0
+      right: 0
+      bottom: 0
+  &__title
+    font-size: 1rem
+    width: 90%
+    font-weight: bold
+  &__ps
+    margin-bottom: 0
+  &__number
+    margin-bottom: 0
+    position: absolute
+    top: 0
+    right: 20px
+    font-size: 2rem
+    color: var(--clr-grey)
+  &-btn-content
+    width: 100%
+  &-info
+    position: absolute
+    top: -8px
+    left: -8px
+    right: -8px
+    bottom: -8px
+    z-index: 10
+    color: var(--clr-black) !important
+    .nes-list
+      margin-left: 10px
+      li
+        margin-bottom: 5px
+    &-close
       position: absolute
-      top: -8px
-      left: -8px
-      right: -8px
-      bottom: -8px
-      z-index: 10
-      color: var(--clr-black) !important
-      .nes-list
-        margin-left: 10px
-        li
-          margin-bottom: 5px
-      &-close
-        position: absolute
-        top: 8px
-        right: 8px
-    &:last-child
-      margin-bottom: 5px !important
+      top: 8px
+      right: 8px
+  &:last-child
+    margin-bottom: 5px !important
   .disable
     color: var(--clr-grey) !important
     .build__avatar-img, .fraise:not(.fraise-bonus)
--- src/components/Store.vue
+++ src/components/Store.vue
@@ -1,36 +1,54 @@
 <template>
   <div class="store nes-container is-rounded is-semi-white">
     <p class="title">Magasin</p>
-    <div class="container-scroll" data-simplebar data-simplebar-auto-hide="false">
-      <div class="store-empty text-center" v-if="storeEmpty">Rien a acheter pour le moment...</div>
+    <div
+      class="container-scroll"
+      data-simplebar
+      data-simplebar-auto-hide="false"
+    >
+      <div class="store-empty text-center" v-if="storeEmpty">
+        Rien à acheter pour le moment...
+      </div>
       <template v-for="(s, i) in store" :key="s.id" class="store-content">
         <template v-for="(store, index) in s.store" :key="index">
-          <tippy 
-            :followCursor="true" 
-            placement="top-start" 
-            duration="0" 
+          <tippy
+            :followCursor="true"
+            placement="top-start"
+            duration="0"
             v-if="!store.purchase && build(s.id).number >= store.number"
           >
-            <button 
-              class="store-btn nes-btn animate__animated animate__fadeInDown"  
+            <button
+              class="store-btn nes-btn animate__animated animate__fadeInDown"
               :class="{
                 'is-success': currency >= build(s.id).price * (index + 2),
-                'is-error': currency <  build(s.id).price * (index + 2)
+                'is-error': currency < build(s.id).price * (index + 2),
               }"
               @click.prevent.stop="purchase(s, store, index)"
             >
-              <div 
-                class="store__avatar" 
-                :style="{backgroundImage: `url(${require(`@/assets/images/builds/unites-production-${i+1}.png`)})` }">
-              </div>
+              <div
+                class="store__avatar"
+                :style="{
+                  backgroundImage: `url(${require(`@/assets/images/builds/unites-production-${
+                    i + 1
+                  }.png`)})`,
+                }"
+              ></div>
             </button>
 
             <template #content>
               <div class="nes-btn">
                 <div class="store-tooltip">
-                  {{build(s.id).name}} deux fois plus efficace !
+                  {{ build(s.id).name }} deux fois plus efficace !
                 </div>
-                <span class="nes-text" :class="{'is-success': currency >= build(s.id).price * (index + 2), 'is-error': currency < build(s.id).price * (index + 2)}">{{renderNumeral(build(s.id).price * (index + 2))}}<span class="fraise small-fraise"></span></span>
+                <span
+                  class="nes-text"
+                  :class="{
+                    'is-success': currency >= build(s.id).price * (index + 2),
+                    'is-error': currency < build(s.id).price * (index + 2),
+                  }"
+                  >{{ renderNumeral(build(s.id).price * (index + 2))
+                  }}<span class="fraise small-fraise"></span
+                ></span>
               </div>
             </template>
           </tippy>
@@ -41,75 +59,75 @@
 </template>
 
 <script>
-import 'simplebar'
-import 'simplebar/dist/simplebar.css'
+import "simplebar"
+import "simplebar/dist/simplebar.css"
 export default {
-  name: 'Store',
+  name: "Store",
   props: {
     store: Array,
     builds: Array,
     currency: Number,
-    storeEmpty: Boolean
+    storeEmpty: Boolean,
   },
   methods: {
-    build(id){
-      return this.builds.filter(b => b.id == id)[0]
+    build(id) {
+      return this.builds.filter((b) => b.id == id)[0]
     },
-    purchase(store, currentStore, index){
-      if (this.currency < this.build(store.id).price * (index + 2)) return;
-      this.$emit('buy', this.build(store.id).price * (index + 2))
-      this.$emit('purchaseStoreSucc')
+    purchase(store, currentStore, index) {
+      if (this.currency < this.build(store.id).price * (index + 2)) return
+      this.$emit("buy", this.build(store.id).price * (index + 2))
+      this.$emit("purchaseStoreSucc")
       this.build(store.id).inc *= 2
       this.build(store.id).stars += 1
-      this.$emit('calcCps')
-      this.$emit('checkStore')
+      this.$emit("calcCps")
+      this.$emit("checkStore")
       currentStore.purchase = true
     },
-    renderNumeral(val){
-      if(val >= 1000000) return numeral(val).format('0.000a')
-      return numeral(val).format('0,0.0')
+    renderNumeral(val) {
+      if (val >= 1000000) return numeral(val).format("0.000a")
+      return numeral(val).format("0,0.0")
     },
-  }
+  },
 }
 </script>
 
 <style lang="sass" scoped>
-  .container-scroll
-    height: 71px !important // 172
-    margin: 0 -10px
-  .store-empty
-    margin: 0 10px
-  .store
-    margin-bottom: 20px !important
-    &-tooltip
-      border-bottom: 3px dashed var(--clr-black)
-      margin-bottom: 5px
-      padding-bottom: 5px
-      font-size: .8rem
-    &-content
-      display: inline-flex
-      flex-wrap: wrap 
-      gap: 15px
-      margin-bottom: 15px
-    &-btn
-      width: min(50px, 10%)
-      min-height: 40px
-      display: inline-flex
-      justify-content: center
-      align-items: center
-      aspect-ratio: 1
-      padding: 0 !important
-      margin: 10px
-    &__avatar
-      width: 40px
-      height: 40px
-      background-size: cover
-      background-repeat: no-repeat
-      background-position: center center
-      filter: hue-rotate(60deg) sepia(78%)
-      border: 3px solid var(--clr-black)
+.container-scroll
+  height: 71px !important // 172
+  margin: 0 -10px
+.store-empty
+  margin: 0 10px
+.store
+  margin-bottom: 20px !important
+  &-tooltip
+    border-bottom: 3px dashed var(--clr-black)
+    margin-bottom: 5px
+    padding-bottom: 5px
+    font-size: .8rem
+  &-content
+    display: inline-flex
+    flex-wrap: wrap
+    gap: 15px
+    margin-bottom: 15px
+  &-btn
+    width: min(50px, 10%)
+    min-height: 40px
+    display: inline-flex
+    justify-content: center
+    align-items: center
+    aspect-ratio: 1
+    padding: 0 !important
+    margin: 10px
+  &__avatar
+    width: 40px
+    height: 40px
+    background-size: cover
+    background-repeat: no-repeat
+    background-position: center center
+    filter: hue-rotate(60deg) sepia(78%)
+    border: 3px solid var(--clr-black)
 
-      display: flex
-      justify-content: center
-      align-items: center
+    display: flex
+    justify-content: center
+    align-items: center
 </style>
